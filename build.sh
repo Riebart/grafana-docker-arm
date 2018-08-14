b96430cab164c71f0a6e63ffa7661f9f87e6b14a
Mike Himbeault <mike@riebart.ca>
2018-08-14 15:45:39 -0500

    Bugfixes and better behaviour when the target image already exists
    - Fixed some bugs in implicit architecture identification
    - Silence stdout, only outputting the built image name at the end
      - All other output goes to stderr
    - If the image being built already exists, don't build it. Emits an empty line to stdout.

--- build.sh
+++ build.sh
@@ -20,28 +20,32 @@ then
         echo "Unknown architecture"
         exit 1
     fi
+fi
+
+if [ "$1" == "-h" ]
+then
+    echo 'Usage: build.sh [armv7|arm64] [Grafana version >= 5.2.0]'
+    echo "  If no architecture is provided, or is an empty string, it is autodetected from uname"
+    echo "  If no Grafana version is provided, the latest stable release is used"
+    echo "  If the provided Grafana version is \"nightly\", then the latest nightly build is used."
+    exit 2
 else
-    if [ "$1" == "-h" ]
+    if [ "$arch" == "" ]
     then
-        echo 'Usage: build.sh [armv7|arm64] [Grafana version >= 5.2.0]'
-        echo "  If no architecture is provided, or is an empty string, it is autodetected from uname"
-        echo "  If no Grafana version is provided, the latest stable release is used"
-        echo "  If the provided Grafana version is \"nightly\", then the latest nightly build is used."
-        exit 2
-    else
         arch="$1"
-        if [ $# -gt 1 ]
+    fi
+
+    if [ $# -gt 1 ]
+    then
+        if [ "$2" == "nightly" ]
         then
-            if [ "$2" == "nightly" ]
-            then
-                nightly_version=$(allowed_versions | tail -n1)
-                grafana_download_url="https://grafana.com/grafana/download/${nightly_version}?platform=arm"
-            else
-                grafana_download_url="https://grafana.com/grafana/download/${2}?platform=arm"
-            fi
+            nightly_version=$(allowed_versions | tail -n1)
+            grafana_download_url="https://grafana.com/grafana/download/${nightly_version}?platform=arm"
         else
-            grafana_download_url="https://grafana.com/grafana/download?platform=arm"
+            grafana_download_url="https://grafana.com/grafana/download/${2}?platform=arm"
         fi
+    else
+        grafana_download_url="https://grafana.com/grafana/download?platform=arm"
     fi
 fi
 
@@ -57,6 +61,13 @@ fi
 # Get the version identified in the tarball:
 version=$(basename "$tarball" | sed "s/grafana-\\(.*\\)\\.linux-${arch}.tar.gz/\\1/")
 
+# Check to see if the image we're going to build exists, if so do nothing
+if [ `docker images -aq grafana:${version}-${arch} | wc -l` -gt 0 ]
+then
+    echo ""
+    exit 0
+fi
+
 # Pull down the Dockerfile and entrypoint script from the GitHub repo, either from the tagged version (if it exists), or the master branch otherwise.
 if wget -O Dockerfile "https://raw.githubusercontent.com/grafana/grafana/v${version}/packaging/docker/Dockerfile"
 then
@@ -71,7 +82,11 @@ chmod +x run.sh
 # Pull down the tarball
 wget --no-clobber "$tarball"
 
-docker build --build-arg GRAFANA_TGZ=`basename "$tarball"` -t grafana:${version}-${arch} .
+# Build the image, pushing output to stderr
+docker build --build-arg GRAFANA_TGZ=`basename "$tarball"` -t grafana:${version}-${arch} . >&2
 
 # Some housekeeping cleanup
 rm `basename "$tarball"` Dockerfile run.sh
+
+# Emit the newly built docker image to stdout
+echo "grafana:${version}-${arch}"
